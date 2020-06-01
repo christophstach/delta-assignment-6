4ced2d118343ad29f002e7bbe8480e52a523d5f6
Christoph Stach <christoph.stach@gmail.com>
2020-06-01 11:54:42 +0800

    finished task 1

---
+++ task1.m
@@ -0,0 +1,46 @@
+load('MNISTModel.mat');
+
+
+outputs = [];
+targets = [];
+
+for i = 0:9
+    directory = './sylvia_mnist/' + string(i) + '/';
+    files = dir(directory + '*.jpg');
+    for o = 1:length(files)
+        name = files(o).name;
+        image = imread(directory + name);
+        image = rgb2gray(image);
+        imageSize = size(image);
+        
+        size(image);
+
+        if imageSize(1) < imageSize(2)
+            image = imresize(image, [28 NaN]);   
+        else
+            image = imresize(image, [NaN 28]);
+        end
+
+        cropper = centerCropWindow2d(size(image), [28, 28]);
+        image = imcrop(image, cropper);
+        output = net.classify(image);
+
+        outputs = [outputs output];
+        targets = [targets categorical(i)];
+    end
+
+    
+    
+    
+
+end
+
+
+t = table(targets.', outputs.');
+
+plotconfusion(targets, outputs);
+fig = uifigure;
+uitable(fig, 'Data', t, 'ColumnName', {'Targets', 'Outputs'});
+
+
+
